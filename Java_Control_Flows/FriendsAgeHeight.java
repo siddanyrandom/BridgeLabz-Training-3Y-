5a31867ca522580f7a5eb258ee3cff5f118104f2
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-08-24 23:28:17 +0530

    Added program to find youngest and tallest among Amar, Akbar, and Anthony

---
+++ Java_Control_Flows/FriendsAgeHeight.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class FriendsAgeHeight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+
+        // Input ages and heights
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter age of " + names[i] + ": ");
+            ages[i] = sc.nextInt();
+            System.out.println("Enter height (in cm) of " + names[i] + ": ");
+            heights[i] = sc.nextDouble();
+        }
+
+        // Find youngest
+        int youngestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+        }
+
+        // Find tallest
+        int tallestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        // Display results
+        System.out.println("\n===== Results =====");
+        System.out.println("Youngest Friend: " + names[youngestIndex] + " (Age: " + ages[youngestIndex] + ")");
+        System.out.println("Tallest Friend: " + names[tallestIndex] + " (Height: " + heights[tallestIndex] + " cm)");
+
+        sc.close();
+    }
+}
