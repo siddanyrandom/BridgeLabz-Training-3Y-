2fe5270daf0efb8d2d4c14aa3172d13fa416e97e
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-09-09 10:33:08 +0530

    Create divisibiltychecker.java

---
+++ Java_Control_Flows/level1/divisibiltychecker.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DivisibilityChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        
+        int number = scanner.nextInt();
+
+        boolean isDivisible = (number % 5 == 0);
+
+        System.out.println("Is the number " + number + " divisible by 5? " + (isDivisible ? "Yes" : "No"));
+
+        scanner.close();
+    }
+}
