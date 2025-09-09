60c8575b5efa3e1154ebf94d5d13681eb43ebdb4
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-09-09 12:53:56 +0530

    Create SpringSeason.java

---
+++ Java_Methods_Practice/level1/SpringSeason.java
@@ -0,0 +1,24 @@
+public class SpringSeason {
+    public static void main(String[] args) {
+        int month = Integer.parseInt(args[0]);
+        int day = Integer.parseInt(args[1]);
+
+        if (isSpring(month, day)) {
+            System.out.println("Its a Spring Season");
+        } else {
+            System.out.println("Not a Spring Season");
+        }
+    }
+
+    public static boolean isSpring(int month, int day) {
+        if (month < 3 || month > 6) {
+            return false;
+        } else if (month == 3 && day < 20) {
+            return false;
+        } else if (month == 6 && day > 20) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
