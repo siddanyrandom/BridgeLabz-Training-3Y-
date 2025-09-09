0c5c59b304c33c6ad18c10f6dae2149a2b312b6b
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-09-09 10:46:55 +0530

    Create NumberSignChecker.java

---
+++ Java_Control_Flows/level1/NumberSignChecker.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NumberSignChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println("The number " + number + " is positive.");
+        } else if (number < 0) {
+            System.out.println("The number " + number + " is negative.");
+        } else {
+            System.out.println("The number " + number + " is zero.");
+        }
+
+        scanner.close();
+    }
+}
