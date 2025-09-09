8cbe108712de5fb86e1f1320ea7926b74fee4ffa
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-09-09 12:35:23 +0530

    Create HarshadNumber.java

---
+++ Java_Control_Flows/level3/HarshadNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int sum = 0;
+        int temp = number;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += digit;
+            temp = temp / 10;
+        }
+
+        if (number % sum == 0) {
+            System.out.println(number + " is a Harshad Number");
+        } else {
+            System.out.println(number + " is Not a Harshad Number");
+        }
+
+        sc.close();
+    }
+}
