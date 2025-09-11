628f153a77a3f98f13f8dcc083d3fef087f56595
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-09-11 15:24:08 +0530

    Create NumberChecker2.java

---
+++ Java_Methods_Practice/level3/NumberChecker2.java
@@ -0,0 +1,76 @@
+```java
+import java.util.*;
+
+public class NumberChecker {
+
+    public static int countDigits(int num) {
+        return String.valueOf(Math.abs(num)).length();
+    }
+
+    public static int[] digitsArray(int num) {
+        String s = String.valueOf(Math.abs(num));
+        int[] digits = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            digits[i] = s.charAt(i) - '0';
+        }
+        return digits;
+    }
+
+    public static int sumOfDigits(int[] digits) {
+        int sum = 0;
+        for (int d : digits) {
+            sum += d;
+        }
+        return sum;
+    }
+
+    public static int sumOfSquares(int[] digits) {
+        int sum = 0;
+        for (int d : digits) {
+            sum += Math.pow(d, 2);
+        }
+        return sum;
+    }
+
+    public static boolean isHarshad(int num, int[] digits) {
+        int sum = sumOfDigits(digits);
+        return sum != 0 && num % sum == 0;
+    }
+
+    public static int[][] digitFrequency(int[] digits) {
+        int[][] freq = new int[10][2];
+        for (int i = 0; i < 10; i++) {
+            freq[i][0] = i;
+            freq[i][1] = 0;
+        }
+        for (int d : digits) {
+            freq[d][1]++;
+        }
+        return freq;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int[] digits = digitsArray(num);
+
+        System.out.println("Number of digits: " + countDigits(num));
+        System.out.println("Digits array: " + Arrays.toString(digits));
+        System.out.println("Sum of digits: " + sumOfDigits(digits));
+        System.out.println("Sum of squares of digits: " + sumOfSquares(digits));
+        System.out.println("Harshad number: " + isHarshad(num, digits));
+
+        int[][] freq = digitFrequency(digits);
+        System.out.println("Digit frequencies:");
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i][1] > 0) {
+                System.out.println("Digit " + freq[i][0] + ": " + freq[i][1]);
+            }
+        }
+
+        sc.close();
+    }
+}
+```
