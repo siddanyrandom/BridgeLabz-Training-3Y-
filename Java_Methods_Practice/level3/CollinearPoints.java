6665c9bfba78002bad5d4b31b50d2359a081bd8a
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-09-11 15:32:45 +0530

    Create CollinearPoints.java

---
+++ Java_Methods_Practice/level3/CollinearPoints.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class CollinearPoints {
+
+    public static boolean areCollinearSlope(double x1, double y1, double x2, double y2, double x3, double y3) {
+        double slopeAB = (y2 - y1) / (x2 - x1);
+        double slopeBC = (y3 - y2) / (x3 - x2);
+        double slopeAC = (y3 - y1) / (x3 - x1);
+        return slopeAB == slopeBC && slopeBC == slopeAC;
+    }
+
+    public static boolean areCollinearArea(double x1, double y1, double x2, double y2, double x3, double y3) {
+        double area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+        return area == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter x1 y1: ");
+        double x1 = sc.nextDouble();
+        double y1 = sc.nextDouble();
+
+        System.out.print("Enter x2 y2: ");
+        double x2 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+
+        System.out.print("Enter x3 y3: ");
+        double x3 = sc.nextDouble();
+        double y3 = sc.nextDouble();
+
+        boolean collinearSlope = areCollinearSlope(x1, y1, x2, y2, x3, y3);
+        boolean collinearArea = areCollinearArea(x1, y1, x2, y2, x3, y3);
+
+        System.out.println("Collinear (Slope Method): " + collinearSlope);
+        System.out.println("Collinear (Area Method): " + collinearArea);
+
+        sc.close();
+    }
+}
