b464017aa11b44565f2a39b9a8dc09bbf893dafa
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-09-11 15:29:38 +0530

    Create FactorAnalyzer.java

---
+++ Java_Methods_Practice/level3/FactorAnalyzer.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class FactorAnalyzer {
+
+    public static int[] findFactors(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) count++;
+        }
+
+        int[] factors = new int[count];
+        int index = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                factors[index++] = i;
+            }
+        }
+        return factors;
+    }
+
+    public static int greatestFactor(int[] factors) {
+        int max = factors[0];
+        for (int f : factors) {
+            if (f > max) max = f;
+        }
+        return max;
+    }
+
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int f : factors) sum += f;
+        return sum;
+    }
+
+    public static long productOfFactors(int[] factors) {
+        long product = 1;
+        for (int f : factors) product *= f;
+        return product;
+    }
+
+    public static long productOfCubes(int[] factors) {
+        long product = 1;
+        for (int f : factors) product *= Math.pow(f, 3);
+        return product;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int[] factors = findFactors(num);
+
+        System.out.println("Factors: " + Arrays.toString(factors));
+        System.out.println("Greatest factor: " + greatestFactor(factors));
+        System.out.println("Sum of factors: " + sumOfFactors(factors));
+        System.out.println("Product of factors: " + productOfFactors(factors));
+        System.out.println("Product of cubes of factors: " + productOfCubes(factors));
+
+        sc.close();
+    }
+}
