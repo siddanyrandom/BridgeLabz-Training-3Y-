404f1cb9411cb5b38fee35e1e385ae92c75aaaea
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-09-13 23:40:51 +0530

    Create Refuelable.java

---
+++ INHERITANCE/Hybrid Inheritance/Refuelable.java
@@ -0,0 +1,72 @@
+
+interface Refuelable {
+    void refuel();
+}
+
+class Vehicle {
+    String model;
+    int maxSpeed;
+
+    Vehicle(String model, int maxSpeed) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+    }
+
+    void displayInfo() {
+        System.out.println("Model: " + model);
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+    }
+}
+
+class ElectricVehicle extends Vehicle {
+    int batteryCapacity; // in kWh
+
+    ElectricVehicle(String model, int maxSpeed, int batteryCapacity) {
+        super(model, maxSpeed);
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    void charge() {
+        System.out.println(model + " is charging. Battery capacity: " + batteryCapacity + " kWh");
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Battery Capacity: " + batteryCapacity + " kWh");
+    }
+}
+
+class PetrolVehicle extends Vehicle implements Refuelable {
+    double fuelTankCapacity; // in liters
+
+    PetrolVehicle(String model, int maxSpeed, double fuelTankCapacity) {
+        super(model, maxSpeed);
+        this.fuelTankCapacity = fuelTankCapacity;
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println(model + " is refueling. Fuel tank capacity: " + fuelTankCapacity + " liters");
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Fuel Tank Capacity: " + fuelTankCapacity + " liters");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        ElectricVehicle ev = new ElectricVehicle("Tesla Model 3", 250, 75);
+        PetrolVehicle pv = new PetrolVehicle("Toyota Corolla", 180, 50);
+
+        ev.displayInfo();
+        ev.charge();
+        System.out.println();
+        pv.displayInfo();
+        pv.refuel();
+    }
+}
+```
