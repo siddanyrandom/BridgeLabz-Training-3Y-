50771ed0aa7027ec01f36b368e10a41bdfe01bf7
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-09-13 23:36:14 +0530

    Create Course.java

---
+++ INHERITANCE/Multilevel Inheritance/Course.java
@@ -0,0 +1,60 @@
+```java
+class Course {
+    String courseName;
+    int duration; // in hours
+
+    Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    void displayInfo() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Duration: " + duration + " hours");
+    }
+}
+
+class OnlineCourse extends Course {
+    String platform;
+    boolean isRecorded;
+
+    OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Platform: " + platform);
+        System.out.println("Recorded: " + isRecorded);
+    }
+}
+
+class PaidOnlineCourse extends OnlineCourse {
+    double fee;
+    double discount; // in percentage
+
+    PaidOnlineCourse(String courseName, int duration, String platform, boolean isRecorded, double fee, double discount) {
+        super(courseName, duration, platform, isRecorded);
+        this.fee = fee;
+        this.discount = discount;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Fee: $" + fee);
+        System.out.println("Discount: " + discount + "%");
+        System.out.println("Final Price: $" + (fee - (fee * discount / 100)));
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        PaidOnlineCourse course = new PaidOnlineCourse("Java Programming", 40, "Udemy", true, 200, 20);
+        course.displayInfo();
+    }
+}
+```
