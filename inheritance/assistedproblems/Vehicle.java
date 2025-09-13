83b3e04af0fa533caf4da2afbed7ecfeec695509
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-09-13 23:28:02 +0530

    Create Vehicle.java

---
+++ inheritance/assistedproblems/Vehicle.java
@@ -0,0 +1,73 @@
+
+class Vehicle {
+    int maxSpeed;
+    String fuelType;
+
+    Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    void displayInfo() {
+        System.out.println("Max Speed: " + maxSpeed);
+        System.out.println("Fuel Type: " + fuelType);
+    }
+}
+
+class Car extends Vehicle {
+    int seatCapacity;
+
+    Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+    }
+}
+
+class Truck extends Vehicle {
+    double loadCapacity;
+
+    Truck(int maxSpeed, String fuelType, double loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+class Motorcycle extends Vehicle {
+    boolean hasSidecar;
+
+    Motorcycle(int maxSpeed, String fuelType, boolean hasSidecar) {
+        super(maxSpeed, fuelType);
+        this.hasSidecar = hasSidecar;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Sidecar: " + hasSidecar);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = {
+            new Car(180, "Petrol", 5),
+            new Truck(120, "Diesel", 15),
+            new Motorcycle(160, "Petrol", false)
+        };
+
+        for (Vehicle v : vehicles) {
+            v.displayInfo();
+            System.out.println();
+        }
+    }
+}
+```
