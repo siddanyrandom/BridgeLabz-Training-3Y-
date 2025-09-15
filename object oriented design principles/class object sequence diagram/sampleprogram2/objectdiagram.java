4b1b411631bb8a4afaed738384c1d13d6f2b0800
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-09-15 11:30:59 +0530

    Create objectdiagram.java

---
+++ object oriented design principles/class object sequence diagram/sampleprogram2/objectdiagram.java
@@ -0,0 +1,22 @@
+:Customer alice
+-------------------------------
+name = "Alice"
+id   = "C101"
+products → {apples, milk}
+-------------------------------
+
+:Product apples
+-------------------------------
+name  = "Apples"
+quantity = 2 kg
+pricePerKg = $3
+totalPrice = $6
+-------------------------------
+
+:Product milk
+-------------------------------
+name  = "Milk"
+quantity = 1 liter
+pricePerLiter = $2
+totalPrice = $2
+-------------------------------
