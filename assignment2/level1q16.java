6533dab6acbdd7aa46fb074a33cacd184d9bfaaf
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-11-09 22:55:42 +0530

    Add HandshakeCalculator to compute handshakes

---
+++ assignment2/level1q16.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class HandshakeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        int handshakes = (n * (n - 1)) / 2;
+        System.out.println("Maximum number of handshakes possible: " + handshakes);
+    }
+}
