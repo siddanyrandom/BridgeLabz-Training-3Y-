3188ff2b9da8c7609c9b7bb2001aa20b30220d87
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-11-09 22:35:00 +0530

    Create q2.java

---
+++ assignment1/q2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+        System.out.println("Sum: " + (a + b));
+    }
+}
