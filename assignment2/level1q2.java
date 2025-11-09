7ccdc6292bf94349a421d648716be6f69e999058
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-11-09 22:47:22 +0530

    Add SamAverage class to calculate average marks

---
+++ assignment2/level1q2.java
@@ -0,0 +1,7 @@
+class SamAverage {
+    public static void main(String[] args) {
+        double maths = 94, physics = 95, chemistry = 96;
+        double average = (maths + physics + chemistry) / 3;
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
