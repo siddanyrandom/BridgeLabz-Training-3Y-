d5c762bff9c75ff5cc5d0efd5b659ccd71b40a14
siddanyrandom <siddharthasaxena11a@gmail.com>
2025-11-09 22:57:36 +0530

    Add BasicCalculator for basic arithmetic operations

---
+++ assignment2/level2q1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double n1 = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double n2 = sc.nextDouble();
+        double add = n1 + n2;
+        double sub = n1 - n2;
+        double mul = n1 * n2;
+        double div = n1 / n2;
+        System.out.println("The addition, subtraction, multiplication and division value of 2 numbers " + n1 + " and " + n2 + " is " + add + ", " + sub + ", " + mul + ", and " + div);
+    }
+}
